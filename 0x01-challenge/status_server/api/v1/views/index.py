70a7f985368e662d03136ead35a265b6682ec882
senseiuc <dhinhojboy@gmail.com>
2023-04-09 15:31:01 +0000

    fix status

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -2,9 +2,9 @@
 """ Index view
 """
 from flask import jsonify
+from flask import Blueprint
 
-from api.v1.views import app_views
-
+app_views = Blueprint('app_views', __name__)
 
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
