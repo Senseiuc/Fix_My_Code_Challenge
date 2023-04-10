7e14e41d5414b5151bbd716b8deb1d0d2c496b5d
senseiuc <dhinhojboy@gmail.com>
2023-04-10 05:10:14 +0000

    square fix perimeter

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" A SQUARE MODULE """
+"""
+A SQUARE MODULE
+"""
 
 
 class Square():
@@ -39,7 +41,7 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def PerimiterOfMySquare(self):
+    def perimiterOfMySquare(self):
         """ perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -53,4 +55,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PerimiterOfMySquare())
+    print(s.perimiterOfMySquare())
